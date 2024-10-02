85b8d71b8f74e4c1911cf902c4f9fafb12cd87c6
Björn Wilmsmann <bjoern@bjoernkw.com>
2024-10-02 11:53:25 +0200

    Sample solution for day 6

--- src/test/java/de/workshops/bookshelf/book/BookRestControllerIntegrationTest.java
+++ src/test/java/de/workshops/bookshelf/book/BookRestControllerIntegrationTest.java
@@ -152,13 +152,6 @@ class BookRestControllerIntegrationTest {
         .usingRecursiveComparison()
         .ignoringFields("id")
         .isEqualTo(expectedBook);
-
-    // Restore previous database state by deleting the book again.
-    mockMvc.perform(MockMvcRequestBuilders.delete("/book/{isbn}", isbn)
-            .contentType(MediaType.APPLICATION_JSON)
-            .with(csrf()))
-        .andDo(MockMvcResultHandlers.print())
-        .andExpect(MockMvcResultMatchers.status().isOk());
   }
 
   @Test
